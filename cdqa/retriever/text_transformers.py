4c2711349ac48000d19f33abf4866386f7e7d2eb
Andre Farias <andre.lmfarias@gmail.com>
2019-09-05 09:38:57 +0200

    added MB25 text transformer

---
+++ cdqa/retriever/text_transformers.py
@@ -0,0 +1,120 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2018, Sho IIZUKA
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+import numpy as np
+import scipy.sparse as sp
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_is_fitted
+from sklearn.feature_extraction.text import _document_frequency
+
+class BM25Transformer(BaseEstimator, TransformerMixin):
+    """
+    Parameters
+    ----------
+    use_idf : boolean, optional (default=True)
+    k1 : float, optional (default=2.0)
+    b : float, optional (default=0.75)
+    References
+    ----------
+    Okapi BM25: a non-binary model - Introduction to Information Retrieval
+    http://nlp.stanford.edu/IR-book/html/htmledition/okapi-bm25-a-non-binary-model-1.html
+    """
+    def __init__(self, use_idf=True, k1=2.0, b=0.75):
+        self.use_idf = use_idf
+        self.k1 = k1
+        self.b = b
+
+    def fit(self, X):
+        """
+        Parameters
+        ----------
+        X : sparse matrix, [n_samples, n_features]
+            document-term matrix
+        """
+        if not sp.issparse(X):
+            X = sp.csc_matrix(X)
+        if self.use_idf:
+            n_samples, n_features = X.shape
+            df = _document_frequency(X)
+            idf = np.log((n_samples - df + 0.5) / (df + 0.5))
+            self._idf_diag = sp.spdiags(idf, diags=0, m=n_features, n=n_features)
+        return self
+
+    def transform(self, X, copy=True):
+        """
+        Parameters
+        ----------
+        X : sparse matrix, [n_samples, n_features]
+            document-term matrix
+        copy : boolean, optional (default=True)
+        """
+        if hasattr(X, 'dtype') and np.issubdtype(X.dtype, np.float):
+            # preserve float family dtype
+            X = sp.csr_matrix(X, copy=copy)
+        else:
+            # convert counts or binary occurrences to floats
+            X = sp.csr_matrix(X, dtype=np.float64, copy=copy)
+
+        n_samples, n_features = X.shape
+
+        # Document length (number of terms) in each row
+        # Shape is (n_samples, 1)
+        dl = X.sum(axis=1)
+        # Number of non-zero elements in each row
+        # Shape is (n_samples, )
+        sz = X.indptr[1:] - X.indptr[0:-1]
+        # In each row, repeat `dl` for `sz` times
+        # Shape is (sum(sz), )
+        # Example
+        # -------
+        # dl = [4, 5, 6]
+        # sz = [1, 2, 3]
+        # rep = [4, 5, 5, 6, 6, 6]
+        rep = np.repeat(np.asarray(dl), sz)
+        # Average document length
+        # Scalar value
+        avgdl = np.average(dl)
+        # Compute BM25 score only for non-zero elements
+        data = X.data * (self.k1 + 1) / (X.data + self.k1 * (1 - self.b + self.b * rep / avgdl))
+        X = sp.csr_matrix((data, X.indices, X.indptr), shape=X.shape)
+
+        if self.use_idf:
+            check_is_fitted(self, '_idf_diag', 'idf vector is not fitted')
+
+            expected_n_features = self._idf_diag.shape[0]
+            if n_features != expected_n_features:
+                raise ValueError("Input has n_features=%d while the model"
+                                 " has been trained with n_features=%d" % (
+                                     n_features, expected_n_features))
+            # *= doesn't work
+            X = X * self._idf_diag
+
+        return X
