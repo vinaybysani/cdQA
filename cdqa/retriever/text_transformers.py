84c20636cb363fb0fca4155fea2a457a0106a6e1
andrelmfarias <andre.lmfarias@gmail.com>
2019-09-13 17:29:54 +0200

    [WIP] improved BM25

--- cdqa/retriever/text_transformers.py
+++ cdqa/retriever/text_transformers.py
@@ -32,15 +32,22 @@
 import numpy as np
 import scipy.sparse as sp
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.validation import check_is_fitted, check_array
 from sklearn.feature_extraction.text import _document_frequency
-
+from sklearn.preprocessing import normalize
 
 class BM25Transformer(BaseEstimator, TransformerMixin):
     """
     Parameters
     ----------
+     norm : 'l1', 'l2' or None, optional (default='l2')
+        Each output row will have unit norm, either:
+        * 'l2': Sum of squares of vector elements is 1. The cosine
+        similarity between two vectors is their dot product when l2 norm has
+        been applied.
+        * 'l1': Sum of absolute values of vector elements is 1.
     use_idf : boolean, optional (default=True)
+        Enable inverse-document-frequency reweighting
     k1 : float, optional (default=2.0)
     b : float, optional (default=0.75)
     References
@@ -49,7 +56,8 @@ class BM25Transformer(BaseEstimator, TransformerMixin):
     http://nlp.stanford.edu/IR-book/html/htmledition/okapi-bm25-a-non-binary-model-1.html
     """
 
-    def __init__(self, use_idf=True, k1=2.0, b=0.75):
+    def __init__(self, norm='l2',use_idf=True, k1=2.0, b=0.75):
+        self.norm = norm
         self.use_idf = use_idf
         self.k1 = k1
         self.b = b
@@ -61,6 +69,7 @@ class BM25Transformer(BaseEstimator, TransformerMixin):
         X : sparse matrix, [n_samples, n_features]
             document-term matrix
         """
+        X = check_array(X, accept_sparse=('csr', 'csc'))
         if not sp.issparse(X):
             X = sp.csc_matrix(X)
         if self.use_idf:
@@ -124,5 +133,7 @@ class BM25Transformer(BaseEstimator, TransformerMixin):
                 )
             # *= doesn't work
             X = X * self._idf_diag
+
+        X = normalize(X, norm=self.norm, copy=False)
 
         return X
